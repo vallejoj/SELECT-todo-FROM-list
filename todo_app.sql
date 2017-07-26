295cf8600906422d3ae196e383d95477384fc8fd
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-25 15:58:52 -1000

    I believe i am done.

--- todo_app.sql
+++ todo_app.sql
@@ -1,21 +1,14 @@
-DROP DATABANK IF EXISTS todo_app;
+DROP DATABASE IF EXISTS todo_app;
 DROP USER IF EXISTS michael;
 
-CREATE USER michael WITH ENCYPTED PASSWORD 'stonebreaker';
+CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 
 CREATE DATABASE todo_app;
 
 
 \c todo_app
 
-CREATE TABLE tasks (
-    id  SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    description text DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP,
-    completed  BOOLEAN DEFAULT FALSE
-);
+CREATE TABLE tasks (id  SERIAL PRIMARY KEY, title VARCHAR(100) NOT NULL, description text DEFAULT NULL, created_at TIMESTAMP DEFAULT NOW(), updated_at TIMESTAMP, completed  BOOLEAN);
 
 ALTER TABLE tasks DROP COLUMN completed;
 
@@ -23,10 +16,9 @@ ALTER TABLE tasks ADD COLUMN completed_at TIMESTAMP DEFAULT NULL;
 
 ALTER TABLE tasks ALTER COLUMN updated_at SET NOT NULL;
 
-ALTER TABLE tasks ALTER COLUMN updated_at TIMESTAMP;
+ALTER TABLE tasks ALTER COLUMN updated_at TIMESTAMP DEFAULT SET NOT NULL;
 
-INSERT INTO tasks (title, description,created_at, updated_at, completed_at) VALUES('Study SQL', 'Complete th
-is exercise',now(),now(),NULL);
+INSERT INTO tasks (title, description,created_at, updated_at, completed_at) VALUES('Study SQL', 'Complete this exercise',now(),now(),NULL);
 
 INSERT INTO tasks (title, description,created_at, updated_at, completed_at) VALUES('Study PostgreSQL', 'Read all the documentation',now(),now(),NULL);
 
@@ -38,14 +30,15 @@ SELECT title, description FROM tasks WHERE completed_at IS NULL;
 
 SELECT * FROM tasks ORDER BY created_at DESC;
 
-INSERT INTO tasks (title, description) VALUES ('mistake 1',  'another test entry');
+INSERT INTO tasks (title, description, created_at, updated_at, completed_at) VALUES ('mistake 1',  'another test entry',now(),now(),NULL);
 
-INSERT INTO tasks (title, description) VALUES ('mistake 2',  'another test entry');
-
-INSERT INTO tasks (title, description,completed_at) VALUES ('mistake three',  'another test entry', NULL);
+INSERT INTO tasks (title, description, created_at, updated_at, completed_at) VALUES ('mistake 2',  'another test entry',now(),now(),NULL);
 
 
-SELECT title FROM tasks WHERE title "mistake%";
+INSERT INTO tasks (title, description, created_at, updated_at, completed_at) VALUES ('mistake third',  'another test entry',now(),now(),NULL);
+
+
+SELECT title FROM tasks WHERE title LIKE 'mistake%';
 
 DELETE FROM tasks WHERE title = 'mistake'
 
